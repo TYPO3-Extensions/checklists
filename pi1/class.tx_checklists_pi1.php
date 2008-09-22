26ecd56f511f137cd8c7bf4052d1397a956d3481
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-22 21:12:54 +0000

    Improved list rendering Added initialisation of results list
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/checklists/trunk@12224 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_checklists_pi1.php
+++ pi1/class.tx_checklists_pi1.php
@@ -57,11 +57,12 @@ class tx_checklists_pi1 extends tslib_pibase {
 		$this->conf = $conf;
 //		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
-t3lib_div::debug($this->piVars);
+//t3lib_div::debug($this->piVars);
+//t3lib_div::debug($GLOBALS['TSFE']->fe_user->user[$GLOBALS['TSFE']->fe_user->username_column]);
 
 			// If a checklist form has been submitted, handle the results
 		if (isset($this->piVars['submit'])) {
-			
+			$this->saveChecks();
 		}
 
 			// If no id is defined, display the list of all checklists
@@ -150,9 +151,12 @@ t3lib_div::debug($this->piVars);
 				}
 				else {
 						// Group items by parent id, so that they can be related to their group easily
+						// We also build a list of items uid's (this is used further down)
 					$sortedItems = array();
+					$itemUids = array();
 					foreach ($items as $row) {
 						$parentId = $row['parentid'];
+						$itemUids[] = $row['uid'];
 						if (!isset($sortedItems[$parentId])) $sortedItems[$parentId] = array();
 						$sortedItems[$parentId][] = $row;
 					}
@@ -174,7 +178,8 @@ t3lib_div::debug($this->piVars);
 						if (isset($sortedItems[$realGroupId])) {
 							$groupItemContents = '';
 							foreach ($sortedItems[$realGroupId] as $anItem) {
-								$itemMarkers['###CHECKBOX###'] = $this->cObj->stdWrap($anItem['uid'], $this->conf['listView.']['item.']['checkbox.']);
+								$GLOBALS['TSFE']->register['item_uid'] = $anItem['uid'];
+								$itemMarkers['###CHECKBOX###'] = $this->cObj->stdWrap('', $this->conf['listView.']['item.']['checkbox.']);
 								$itemMarkers['###TITLE###'] = $this->cObj->stdWrap($anItem['title'], $this->conf['listView.']['item.']['title.']);
 								$itemMarkers['###DESCRIPTION###'] = $this->cObj->stdWrap($anItem['description'], $this->conf['listView.']['item.']['description.']);
 								$itemMarkers['###USER###'] = $this->cObj->stdWrap('', $this->conf['listView.']['item.']['user.']);
@@ -184,6 +189,8 @@ t3lib_div::debug($this->piVars);
 						$listContent .= $groupContent;
 					}
 					$content .= $this->cObj->stdWrap($listContent, $this->conf['listView.']['listWrap.']);
+						// After having built a whole list, check that the checklist instance has a properly built results field
+					$this->checkResultsField($instanceInfo, $itemUids);
 				}
 				$content .= $this->cObj->stdWrap($this->pi_getLL('submit'), $this->conf['listView.']['submit.']);
 				$content = $this->cObj->stdWrap($content, $this->conf['listView.']['allWrap.']);
@@ -193,15 +200,81 @@ t3lib_div::debug($this->piVars);
 	}
 
 	/**
+	 * This method is used to check that the results field of the checklist instance
+	 * has a full list of checklist items
+	 *
+	 * @param	array	$instance: DB record of the checklist instance
+	 * @param	array	$items: list of all item uid's found in the list
+	 * @return	void
+	 */
+	protected function checkResultsField($instance, $items) {
+			// If there are no results yet, initialise array
+		if (empty($instance['results'])) {
+			$currentResults = array();
+		}
+			// Otherwise extract array of results from stored XML
+		else {
+			$currentResults = t3lib_div::xml2array($instance['results']);
+				// Get the list of currently stored items
+			$storedUids = array_keys($currentResults);
+				// Check which items are not in the list anymore
+			$removedUids = array_diff($storedUids, $items);
+				// If some items are not in the list anymore, remove them from the results list
+			if (count($removedUids) > 0) {
+				foreach ($removedUids as $uid) {
+					unset($currentResults[$uid]);
+				}
+			}
+		}
+			// Loop on all checklist items and initialise those that don't exist
+		foreach ($items as $uid) {
+			if (!isset($currentResults[$uid])) {
+				$currentResults[$uid] = array('status' => 0, 'user' => '');
+			}
+		}
+			// Store the updated results list into the instance
+		$updates = array('results' => t3lib_div::array2xml($currentResults));
+		$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tx_checklists_instances', "uid = '".$instance['uid']."'", $updates);
+			// Check if the instance has translations, as they must have the same results list
+		$where = $GLOBALS['TCA']['tx_checklists_instances']['ctrl']['transOrigPointerField']." = '".$instance['uid']."' AND pid = '".$instance['pid']."'";
+		$where .= $this->cObj->enableFields('tx_checklists_instances', 1);
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('uid', 'tx_checklists_instances', $where);
+			// Loop on all translations and update them with the same results list
+		if ($res && $GLOBALS['TYPO3_DB']->sql_num_rows($res) > 0) {
+			while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+				$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tx_checklists_instances', "uid = '".$row['uid']."'", $updates);
+			}
+		}
+	}
+
+	/**
 	 * This method stores all the checkboxes that were checked
 	 *
 	 * @return	void
 	 */
 	protected function saveChecks() {
 		// First, get the checklist instance record for updating it
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'tx_checklists_instances', "uid = '".$this->piVars['showUid']."'");
+		$instanceInfo = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+		// Check if there has already been results stored for that list
+		// (this shouldn't happen normally)
+		if (empty($instanceInfo['results'])) {
+			$currentResults = array('items' => array());
+		}
+		// If yes, get the results as an array
+		else {
+			$currentResults = t3lib_div::xml2array($instanceInfo['results']);
+		}
 		// Loop on all submitted checkboxes and set them to done
+		foreach ($this->piVars['items'] as $uid => $value) {
+			$currentResults['items'][$uid] = array('status' => 1, 'user' => 'Bob');
+		}
 		// Check if all items have been completed
 		// Save result to checklist instance
+		$results = t3lib_div::array2xml($currentResults);
+//t3lib_div::debug($results);
+//t3lib_div::debug(t3lib_div::xml2array($results));
+//		$GLOBALS['TYPO3_DB']->exec_UPDATEquery();
 		// Save to translations too, so that status is in sync
 	}
 
