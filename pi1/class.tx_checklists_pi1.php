bc7a43834e3fec34484e2389e5582fcc71753ce1
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-17 16:58:29 +0000

    Follow-up to: Changed code to take only local checklist instances when no page is explicitly selected Removed base class inclusions (rely on autoloading) CGL cleanup Removed Kickstarter fluff
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/checklists/trunk@42273 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_checklists_pi1.php
+++ pi1/class.tx_checklists_pi1.php
@@ -23,14 +23,6 @@
 *
 * $Id$
 ***************************************************************/
-/**
- * [CLASS/FUNCTION INDEX of SCRIPT]
- *
- * Hint: use extdeveval to insert/update function index above.
- */
-
-require_once(PATH_tslib.'class.tslib_pibase.php');
-require_once(t3lib_extMgm::extPath('overlays', 'class.tx_overlays.php'));
 
 
 /**
@@ -44,7 +36,7 @@ class tx_checklists_pi1 extends tslib_pibase {
 	var $prefixId      = 'tx_checklists_pi1';		// Same as class name
 	var $scriptRelPath = 'pi1/class.tx_checklists_pi1.php';	// Path to this script relative to the extension dir.
 	var $extKey        = 'checklists';	// The extension key.
-	var $pi_checkCHash = true;
+	var $pi_checkCHash = TRUE;
 
 	/**
 	 * The main method dispatches the generation of the content to the relevant method given the view parameter
@@ -89,9 +81,9 @@ class tx_checklists_pi1 extends tslib_pibase {
 		$where = '';
 		$referencePage = $GLOBALS['TSFE']->id;
 		if (!empty($this->cObj->data['pages'])) {
-			$referencePage = $this->cObj->data['pages'];
+			$referencePage = intval($this->cObj->data['pages']);
 		}
-		$where = 'pid = ' . intval($this->cObj->data['pages']);
+		$where = 'pid = ' . $referencePage;
 		$rows = tx_overlays::getAllRecordsForTable('*', 'tx_checklists_instances', $where, '', 'title');
 			// Display the list of checklist instances
 		$instanceList = '';
